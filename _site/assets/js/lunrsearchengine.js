369b338d56bd2e8665324909c3a53fbf03cfd660
KOTAKA Yuto <63027250+kotakayuto@users.noreply.github.com>
2021-11-28 18:59:31 +0900

    公開準備

--- _site/assets/js/lunrsearchengine.js
+++ _site/assets/js/lunrsearchengine.js
@@ -1,129 +1,129 @@
 
 var documents = [{
     "id": 0,
-    "url": "/biol-lab-intro/404.html",
+    "url": "/lab-intro/404.html",
     "title": "404",
     "body": "404 Page not found!Please use the search bar from the bottom left or visit our homepage! "
     }, {
     "id": 1,
-    "url": "/biol-lab-intro/about",
+    "url": "/lab-intro/about",
     "title": "Memoirs, a free minimalist Jekyll blogging theme with modern design",
     "body": "This website is a demonstration to see Memoirs Jekyll theme in action. The theme is compatible with Github pages, in fact even this demo itself is created with Github Pages and hosted with Github.  Get Memoirs for Jekyll → "
     }, {
     "id": 2,
-    "url": "/biol-lab-intro/authors",
+    "url": "/lab-intro/authors",
     "title": "Authors",
     "body": "{% for author in site. authors %}                       {% if author[1]. gravatar %}                {% else %}                {% endif %}                  {% if author[1]. web %}                       {% endif %}          {% if author[1]. twitter %}                      {% endif %}          {% if author[1]. email %}                      {% endif %}                                     {{ author[1]. display_name }}:         {{ author[1]. description }}                {% endfor %}"
     }, {
     "id": 3,
-    "url": "/biol-lab-intro/categories",
+    "url": "/lab-intro/categories",
     "title": "Categories",
     "body": ""
     }, {
     "id": 4,
-    "url": "/biol-lab-intro/contact",
+    "url": "/lab-intro/contact",
     "title": "Contact",
     "body": "  Please send your message to {{site. name}}. We will reply as soon as possible!   "
     }, {
     "id": 5,
-    "url": "/biol-lab-intro/",
+    "url": "/lab-intro/",
     "title": "Home",
     "body": "  {% for post in paginator. posts %}    {% include postbox. html %}  {% endfor %}  {% include pagination. html %}"
     }, {
     "id": 6,
-    "url": "/biol-lab-intro/robots.txt",
+    "url": "/lab-intro/robots.txt",
     "title": "",
     "body": "      Sitemap: {{ “sitemap. xml”   absolute_url }}   "
     }, {
     "id": 7,
-    "url": "/biol-lab-intro/page2/",
+    "url": "/lab-intro/page2/",
     "title": "Home",
     "body": "  {% for post in paginator. posts %}    {% include postbox. html %}  {% endfor %}  {% include pagination. html %}"
     }, {
     "id": 8,
-    "url": "/biol-lab-intro/page3/",
+    "url": "/lab-intro/page3/",
     "title": "Home",
     "body": "  {% for post in paginator. posts %}    {% include postbox. html %}  {% endfor %}  {% include pagination. html %}"
     }, {
     "id": 9,
-    "url": "/biol-lab-intro/%E3%81%93%E3%81%AE%E3%82%B5%E3%82%A4%E3%83%88%E3%81%AB%E3%81%A4%E3%81%84%E3%81%A6/",
-    "title": "このサイトについて",
-    "body": "2021/11/25 - サイトの紹介文。研究室配属までの流れ　など "
+    "url": "/lab-intro/%E3%81%93%E3%81%AE%E3%82%B5%E3%82%A4%E3%83%88%E3%81%AB%E3%81%A4%E3%81%84%E3%81%A6/",
+    "title": "研究室選びのポイント",
+    "body": "2021/11/25 - 研究室選びのポイント:  研究室の名前にダマされない     研究室の名前からは想像できないような研究をしていることもありますし、研究室の名前のようなことを本当に研究しているとは限りません。    実際にどのような研究をしているのか     先輩から聞いた話、友達から聞いた噂、今までの授業内容、などの情報だけを当てにしていませんか？研究に関しては先生に直接聞きに行くのが一番です。    研究室で何ができるのか     卒業研究でどのようなことができるのか聞いてみましょう。自分で考えたテーマで研究させてくれる研究室もありますし、大きなテーマに沿ってみんなで協力して研究を進めている研究室もあります。   学外の研究機関から指導を受けられる研究室もあります。   卒研生として配属されたら、どのような研究が可能か、具体的に考えて見ましょう。    研究室の雰囲気も重要です。     本サイトで雰囲気だけでも感じてもらえればと思います。       研究室配属までの流れ:   研究室紹介     各研究室の先輩に直接話を聞けます。    アンケート調査①・②     その時点で各研究室を希望する人数を集計します。    予備調査     アンケート調査の結果や研究室紹介を参考にして、希望の研究室を決定し、提出します。    配属先決定     人数調整などを行い、指導教員の許可をもらった上で配属先が決定します。   Topへ戻る "
     }, {
     "id": 10,
-    "url": "/biol-lab-intro/neumol/",
-    "title": "神経分子機能研究室",
-    "body": "2021/11/24 - 神経分子機能研究室の紹介文 "
+    "url": "/lab-intro/neumol/",
+    "title": "神経分子機能研究室（準備中）",
+    "body": "2021/11/24 - 準備中です。 "
     }, {
     "id": 11,
-    "url": "/biol-lab-intro/devpro/",
-    "title": "発生生物学研究室",
-    "body": "2021/11/23 - 発生生物学研究室の紹介文 "
+    "url": "/lab-intro/devpro/",
+    "title": "発生生物学研究室（準備中）",
+    "body": "2021/11/23 - 準備中です。 "
     }, {
     "id": 12,
-    "url": "/biol-lab-intro/celgen/",
-    "title": "細胞遺伝学研究室",
-    "body": "2021/11/22 - 細胞遺伝学研究室の紹介文 "
+    "url": "/lab-intro/celgen/",
+    "title": "細胞遺伝学研究室（準備中）",
+    "body": "2021/11/22 - 準備中です。 "
     }, {
     "id": 13,
-    "url": "/biol-lab-intro/molgen/",
-    "title": "分子遺伝学研究室",
-    "body": "2021/11/21 - 分子遺伝学研究室の紹介文 "
+    "url": "/lab-intro/molgen/",
+    "title": "分子遺伝学研究室（準備中）",
+    "body": "2021/11/21 - 準備中です。 "
     }, {
     "id": 14,
-    "url": "/biol-lab-intro/horcel/",
-    "title": "植物発生生理学研究室",
-    "body": "2021/11/20 - 植物発生生理学研究室の紹介文 "
+    "url": "/lab-intro/horcel/",
+    "title": "植物発生生理学研究室（準備中）",
+    "body": "2021/11/20 - 準備中です。 "
     }, {
     "id": 15,
-    "url": "/biol-lab-intro/celche/",
-    "title": "細胞生化学研究室",
-    "body": "2021/11/19 - 細胞生化学研究室の紹介文 "
+    "url": "/lab-intro/celche/",
+    "title": "細胞生化学研究室（準備中）",
+    "body": "2021/11/19 - 準備中です。 "
     }, {
     "id": 16,
-    "url": "/biol-lab-intro/evogen/",
-    "title": "進化遺伝学研究室",
-    "body": "2021/11/18 - 進化遺伝学研究室の紹介文 hello "
+    "url": "/lab-intro/evogen/",
+    "title": "進化遺伝学研究室（準備中）",
+    "body": "2021/11/18 - 準備中です。 "
     }, {
     "id": 17,
-    "url": "/biol-lab-intro/neubio/",
-    "title": "神経生物学研究室",
-    "body": "2021/11/17 - 神経生物学研究室の紹介文 "
+    "url": "/lab-intro/neubio/",
+    "title": "神経生物学研究室（準備中）",
+    "body": "2021/11/17 - 準備中です。 "
     }, {
     "id": 18,
-    "url": "/biol-lab-intro/pladev/",
-    "title": "植物環境応答研究室",
-    "body": "2021/11/16 - 植物環境応答研究室の紹介文 "
+    "url": "/lab-intro/pladev/",
+    "title": "植物環境応答研究室（準備中）",
+    "body": "2021/11/16 - 準備中です。 "
     }, {
     "id": 19,
-    "url": "/biol-lab-intro/envmic/",
-    "title": "環境微生物学研究室",
-    "body": "2021/11/15 - 環境微生物学研究室の紹介文 "
+    "url": "/lab-intro/envmic/",
+    "title": "環境微生物学研究室（準備中）",
+    "body": "2021/11/15 - 準備中です。 "
     }, {
     "id": 20,
-    "url": "/biol-lab-intro/anieco/",
-    "title": "動物生態学研究室",
-    "body": "2021/11/14 - 動物生態学研究室の紹介文 "
+    "url": "/lab-intro/anieco/",
+    "title": "動物生態学研究室（準備中）",
+    "body": "2021/11/14 - 準備中です。 "
     }, {
     "id": 21,
-    "url": "/biol-lab-intro/plaeco/",
-    "title": "植物生態学研究室",
-    "body": "2021/11/13 - 植物生態学研究室の紹介文 "
+    "url": "/lab-intro/plaeco/",
+    "title": "植物生態学研究室（準備中）",
+    "body": "2021/11/13 - 準備中です。 "
     }, {
     "id": 22,
-    "url": "/biol-lab-intro/anisys/",
-    "title": "動物系統分類学研究室",
-    "body": "2021/11/12 - 動物系統分類学研究室の紹介文 "
+    "url": "/lab-intro/anisys/",
+    "title": "動物系統分類学研究室（準備中）",
+    "body": "2021/11/12 - 準備中です。 "
     }, {
     "id": 23,
-    "url": "/biol-lab-intro/plasys/",
-    "title": "植物系統分類学研究室",
-    "body": "2021/11/11 - 植物系統分類学研究室の紹介文 "
+    "url": "/lab-intro/plasys/",
+    "title": "植物系統分類学研究室（準備中）",
+    "body": "2021/11/11 - 準備中です。 "
     }, {
     "id": 24,
-    "url": "/biol-lab-intro/muscle/",
-    "title": "運動分子生物学研究室【HPS】",
-    "body": "2021/11/10 - 運動分子生物学研究室【HPS】の紹介文 "
+    "url": "/lab-intro/muscle/",
+    "title": "運動分子生物学研究室【HPS】（準備中）",
+    "body": "2021/11/10 - 準備中です。 "
     }];
 
 var lunr = require('./lib/lunr.js');
